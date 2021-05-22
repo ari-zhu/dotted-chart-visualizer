228a0d8155558098ec18d1703d566cd3aa76dbc3
Ariane Chu <12162-ariane.chu@users.noreply.git.rwth-aachen.de>
2021-05-22 14:21:07 +0200

    "dcv.views dcv(request) updated "

--- UIFramework_pm4py-master/dotted_chart_visualizer/views.py
+++ UIFramework_pm4py-master/dotted_chart_visualizer/views.py
@@ -7,9 +7,17 @@ from os import listdir
 from os.path import isfile, join
 from django.http import HttpResponseRedirect
 from bootstrapdjango import settings
+from filter_functions import setDefault, convert_log_to_df
+import pandas as pd
 
 # Create your views here.
 
 def dcv(request):
-    return render(request, 'dcv.html', {'log_name': settings.EVENT_LOG_NAME})
+    #if
+    #else:
+    default_x_axis_df, default_y_axis_df = setDefault(convert_log_to_df(request))
+    default_x_axis_list = default_x_axis_df.values.tolist()
+    default_y_axis_list = default_y_axis_df.values.tolist()
+    return render(request, 'dcv.html', {'log_name': settings.EVENT_LOG_NAME, 'default_x_axis': default_x_axis_list,
+                                        'default_y_axis': default_y_axis_list})
 
