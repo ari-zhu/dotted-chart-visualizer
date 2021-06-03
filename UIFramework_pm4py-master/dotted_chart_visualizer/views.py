0dc446713efa886739e95c265795fe2d5574670c
Jane-NoLastName <83496485+Jane-NoLastName@users.noreply.github.com>
2021-06-03 23:02:03 +0200

    Update views.py

--- UIFramework_pm4py-master/dotted_chart_visualizer/views.py
+++ UIFramework_pm4py-master/dotted_chart_visualizer/views.py
@@ -26,9 +26,10 @@ def dcv(request):
             default_x_axis_df, default_y_axis_df, default_x_axis_label, default_y_axis_label = setDefault(convertLogToDf(file_dir))
             default_x_axis_list = default_x_axis_df.values.tolist()
             default_y_axis_list = default_y_axis_df.values.tolist()
+            default_axis_list = [default_x_axis_list, default_y_axis_list]
+            default_label_list = [default_x_axis_label, default_y_axis_label]
             log_attribute_list = getAttributeNames(convertLogToDf(file_dir))
-            return render(request, 'dcv.html', {'log_name': settings.EVENT_LOG_NAME, 'default_x_axis_list': default_x_axis_list,
-                                        'default_y_axis_list': default_y_axis_list, 'default_x_axis_label': default_x_axis_label, 'default_y_axis_label': default_y_axis_label,
+            return render(request, 'dcv.html', {'log_name': settings.EVENT_LOG_NAME, 'default_axis_list': default_axis_list, 'default_label_list': default_label_list,
                                                 'attribute_list':log_attribute_list})
         else:
             message = "file not valid or separator in CSV file not recognized"
