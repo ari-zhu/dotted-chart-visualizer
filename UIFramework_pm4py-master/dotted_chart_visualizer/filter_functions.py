d1cf7ceb2bc63cf733e5095eb96366837f7ff748
Ariane Chu <12162-ariane.chu@users.noreply.git.rwth-aachen.de>
2021-06-23 10:25:06 +0200

    fixing conflict, no changes

--- UIFramework_pm4py-master/dotted_chart_visualizer/filter_functions.py
+++ UIFramework_pm4py-master/dotted_chart_visualizer/filter_functions.py
@@ -8,49 +8,56 @@ from django.http import HttpResponse
 from pm4py.objects.log.importer.xes import importer as xes_importer_factory
 from pm4py.objects.conversion.log import converter as log_converter
 from django.shortcuts import render
-from .utils import getCaseLabel, getTimeLabel, getTimeIndex
 import re
 import datetime
+
 
 # returns the number of events of event log df
 def getNumberOfEvents(df):
     return len(df)
 
+
 # returns the number of attributes of event log df
 def getNumberOfAttributes(df):
     return len(df.columns)
+
 
 # returns an list of the attribute names of event log df
 def getAttributeNames(df):
     return list(df.columns.values.tolist())
 
+
 # prints out the attribute names of event log df
 def printAttributeNames(df):
     for col in df.columns:
         print(col)
-        
+
+
 # returns an array that contains the values of a specific attribute of event log df
 # index of the attibute is needed, to do: get attribute by name
 def getAttribute(df, attributeIndex):
     if (attributeIndex < len(df.columns)):
         return df.iloc[:, attributeIndex]
     else:
-        print ("index out of bounds")
+        print("index out of bounds")
 
-# returns the event log sorted by a specific attribute, attribute index needed        
+
+# returns the event log sorted by a specific attribute, attribute index needed
 def sortByAttribute(df, attribute):
-    return df.sort_values(by = [attribute])
+    return df.sort_values(by=[attribute])
+
 
 # reduces the number of events of the event log down to a specific range with start and end index
 def delimitNumberOfEvents(df, startIndex, endIndex):
-    if (startIndex < endIndex) &(startIndex >= 0) &(endIndex < len(df)):
+    if (startIndex < endIndex) & (startIndex >= 0) & (endIndex < len(df)):
         try:
-            return df.iloc[startIndex : endIndex]
+            return df.iloc[startIndex: endIndex]
         except:
             print("Error")
-            
+
+
 # returns only the events of a specific trace with index traceIndex
-def getTrace (df, traceIndex):
+def getTrace(df, traceIndex):
     index = "trace " + str(traceIndex)
     return df.loc[df['case'] == index]
 
@@ -102,20 +109,23 @@ def setDefault(df):
 
     return x_column, y_column, x_label, y_label
 
-def get_unique_values (df, col_name):
+
+def get_unique_values(df, col_name):
     return df[col_name].unique()
 
-def get_Colored_Values (df, color_att, color_val, target_att):
-    #filter rows where color_att is equal to color_val
-    df_color = df.loc [df[color_att] == color_val]
-    #reduce to target column
+
+def get_Colored_Values(df, color_att, color_val, target_att):
+    # filter rows where color_att is equal to color_val
+    df_color = df.loc[df[color_att] == color_val]
+    # reduce to target column
     df_color_col = df_color[target_att]
-    #convert to list
+    # convert to list
     return list(df_color_col.values.tolist())
 
-#returns a list of lists where each inner list is the values of the column specified by target index
-#where the values of this column are equal to a unique value of the color column
-def get_Colored_Col (df, color_att, target_att):
+
+# returns a list of lists where each inner list is the values of the column specified by target index
+# where the values of this column are equal to a unique value of the color column
+def get_Colored_Col(df, color_att, target_att):
     unique_vals = get_unique_values(df, color_att)
     col_list = []
     for val in unique_vals:
@@ -123,14 +133,16 @@ def get_Colored_Col (df, color_att, target_att):
         col_list.append(val_list)
     return col_list
 
-def get_Colored_And_Shaped_Values (df, color_att, color_val, shaped_att, shaped_val, target_att):
-    #filter rows where color_att is equal to color_val
-    df_color = df.loc [df[color_att] == color_val]
-    df_color_shape = df_color.loc [df_color[shaped_att] == shaped_val]
-    #reduce to target column
-    target_col = df_color_shape [target_att]
-    #convert to list
+
+def get_Colored_And_Shaped_Values(df, color_att, color_val, shaped_att, shaped_val, target_att):
+    # filter rows where color_att is equal to color_val
+    df_color = df.loc[df[color_att] == color_val]
+    df_color_shape = df_color.loc[df_color[shaped_att] == shaped_val]
+    # reduce to target column
+    target_col = df_color_shape[target_att]
+    # convert to list
     return list(target_col.values.tolist())
+
 
 def get_Colored_AND_Shaped(df, color_att, shaped_att, target_att):
     unique_colored = get_unique_values(df, color_att)
@@ -143,37 +155,58 @@ def get_Colored_AND_Shaped(df, color_att, shaped_att, target_att):
     return res_list
 
 
-#Sorting Functions
+# Sorting Functions
 def sortByTrace(df):
     caseLabel = getCaseLabel(df)
-    return df.sort_values(by = [caseLabel])
+    return df.sort_values(by=[caseLabel])
+
 
 def sortByTime(df):
     timeLabel = getTimeLabel(df)
-    return df.sort_values(by = [timeLabel])
+    return df.sort_values(by=[timeLabel])
+
 
 def sortByFirstInTrace(df):
-    dfu = get_unique_values (df, getCaseLabel(df))
+    dfu = get_unique_values(df, getCaseLabel(df))
     firstInTraceList = []
     for d in dfu:
-        dfr = df.loc [df[getCaseLabel(df)] == d]
+        dfr = df.loc[df[getCaseLabel(df)] == d]
         firstInTraceList.append(dfr.iloc[0])
     return firstInTraceList
 
+
 def sortByLastInTrace(df):
-    dfu = get_unique_values (df, getCaseLabel(df))
+    dfu = get_unique_values(df, getCaseLabel(df))
     lastInTraceList = []
     for d in dfu:
-        dfr = df.loc [df[getCaseLabel(df)] == d]
+        dfr = df.loc[df[getCaseLabel(df)] == d]
         lastInTraceList.append(dfr.iloc[-1])
     return lastInTraceList
 
-#Converting TimeStamps
+
+def sortyByTraceDuration(df):
+    durationList = []
+    traceList = []
+    dfu = get_unique_values(df, getCaseIndex(df))
+    for d in dfu:
+        dfr = df.loc[df[getCaseLabel(df)] == d]
+        finishTime = pd.to_datetime(dfr.iloc[-1][getTimeIndex(df)])
+        startTime = pd.to_datetime(dfr.iloc[0][getTimeIndex(df)])
+        duration = finishTime - startTime
+        durationList.append(duration)
+        traceList.append(d)
+        trace_duration_df = pd.DataFrame(list(zip(traceList, durationList)), columns=['trace', 'duration'])
+        td_sort = trace_duration_df.sort_values(by='duration', ascending=False)
+    return td_sort.values.tolist()
+
+
+# Converting TimeStamps
 
 def convertStringToDateTime(date_string):
     format = "%Y-%m-%d %H:%M:%S"
     date_time_obj = datetime.datetime.strptime(date_string, format)
     return date_time_obj
+
 
 def convertListOfStringsToDateTime(list_of_strings):
     res_list = []
@@ -182,13 +215,15 @@ def convertListOfStringsToDateTime(list_of_strings):
         res_list.append(dt_obj)
     return res_list
 
-#this one works best
+
+# this one works best
 def convertTimeStamps(df):
     timeIndex = getTimeIndex(df)
-    if(isinstance(df.iloc[0,timeIndex],str)):
+    if (isinstance(df.iloc[0, timeIndex], str)):
         df[getTimeLabel(df)] = pd.to_datetime(df[getTimeLabel(df)])
 
-#returns the values of the time column converted from date time to string 
+
+# returns the values of the time column converted from date time to string
 def convertDateTimeToString(df):
     timeIndex = getTimeIndex(dfy)
     strList = []
@@ -196,7 +231,31 @@ def convertDateTimeToString(df):
         u = v.strftime("%Y-%m-%d %H:%M:%S")
         strList.append(u)
     return strList
-        
 
 
+# renames column names to get prettier names, used for XES files
+def renameXesColumns(df):
+    df = df.rename(columns={getTimeLabel(df): "Time", getCaseLabel(df): "Case"})
+    if ("org:resource" in df.columns):
+        df = df.rename(columns={"org:resource": "Resource"})
+    if ("concept:name" in df.columns):
+        df = df.rename(columns={"concept:name": "Activity"})
+    if ("case:creator in df.columns"):
+        df = df.rename(columns={"case:creator": "Creator"})
+    if ("lifecycle:transition" in df.columns):
+        df = df.rename(columns={"lifecycle:transition": "Lifecycle"})
+    if ("case:description" in df.columns):
+        df = df.rename(columns={"case:description": "Description"})
+    for col in df.columns:
+        if "case:" in col:
+            prefix, colName = col.split(":")
+            if colName.islower():
+                firstLetter = colName[:1].upper()
+                restWord = colName.split([colName[:1]])[1]
+                res = ''.join([firstLetter, restWord])
+                df = df.rename(columns={colName: res})
+            else:
+                df = df.rename(columns={col: colName})
+    return df
 
+# sorts
--- UIFramework_pm4py-master/dotted_chart_visualizer/utils.py
+++ UIFramework_pm4py-master/dotted_chart_visualizer/utils.py
@@ -8,30 +8,33 @@ from .filter_functions import get_unique_values as to_set
 from .filter_functions import getAttributeNames
 from .filter_functions import sortByAttribute as sort_df
 
+
 def extract_attr_log_level(df):
     log_level_attributes = [attr for attr in getAttributeNames(df) if 'case' in attr]
     return log_level_attributes
+
 
 def extract_attr_case_level(df, log_level_attributes):
     case_level_attributes = [attr for attr in getAttributeNames(df) if attr not in log_level_attributes]
     return case_level_attributes
 
-def extract_attribute_value(log_object,event,attribute):
+
+def extract_attribute_value(log_object, event, attribute):
     pass
 
-def convertLogToDf(file_dir):
 
+def convertLogToDf(file_dir):
     name, extension = os.path.splitext(file_dir)
 
     if (extension == ".xes"):
         xes_log = xes_importer_factory.apply(file_dir)
         df_event_log = log_converter.apply(xes_log, variant=log_converter.Variants.TO_DATA_FRAME)
         log_level_attributes = extract_attr_log_level(df_event_log)
-        case_level_attributes = extract_attr_case_level(df_event_log,log_level_attributes)
-        #log_level_attributes = [attr[5:] for attr in log_level_attributes]
+        case_level_attributes = extract_attr_case_level(df_event_log, log_level_attributes)
+        # log_level_attributes = [attr[5:] for attr in log_level_attributes]
         return df_event_log, case_level_attributes, log_level_attributes
 
-    else: #(extension == ".csv"):
+    else:  # (extension == ".csv"):
         df_event_log = pd.read_csv(file_dir)
         if (not checkCommaSeparated(df_event_log)):
             separator = ';'
@@ -39,13 +42,14 @@ def convertLogToDf(file_dir):
                 df_event_log = pd.read_csv(file_dir, sep=separator)
         return df_event_log, getAttributeNames(df_event_log), extract_attr_log_level(df_event_log)
 
-#checks if Dataframe is Comma-Separated, returns True if it is, used in covert_log_to_df function
+
+# checks if Dataframe is Comma-Separated, returns True if it is, used in covert_log_to_df function
 def checkCommaSeparated(df):
-    if len ((df.columns)) != 1:
+    if len((df.columns)) != 1:
         return True
 
 
-#extract user settings from dropdown menu
+# extract user settings from dropdown menu
 def selection(selection_dict):
     selection_list = [selection_dict["xaxis_choice"], selection_dict["yaxis_choice"], None, None]
     axes_only = True
@@ -61,7 +65,8 @@ def selection(selection_dict):
             complete = True
     return axes_only, complete, selection_list
 
-#create lists for labelling of axes, data points for plot, attribute values for legend
+
+# create lists for labelling of axes, data points for plot, attribute values for legend
 def data_points(df, attr_dict):
     axes_only, complete, selection_list = selection(attr_dict)
     labels_list = [selection_list[0], selection_list[1], 'Choose here', 'Choose here']
@@ -71,9 +76,9 @@ def data_points(df, attr_dict):
         return labels_list, data_points_list, []
     elif complete:
         labels_list[2], labels_list[3] = selection_list[2], selection_list[3]
-        data_points_list = [get_Colored_AND_Shaped(df, selection_list[2],selection_list[3],selection_list[0]),
-                            get_Colored_AND_Shaped(df, selection_list[2],selection_list[3],selection_list[1])]
-        legend_list = [to_set(df, selection_list[2]).tolist(), to_set(df,selection_list[3]).tolist()]
+        data_points_list = [get_Colored_AND_Shaped(df, selection_list[2], selection_list[3], selection_list[0]),
+                            get_Colored_AND_Shaped(df, selection_list[2], selection_list[3], selection_list[1])]
+        legend_list = [to_set(df, selection_list[2]).tolist(), to_set(df, selection_list[3]).tolist()]
         return labels_list, data_points_list, legend_list
 
     else:
@@ -86,93 +91,52 @@ def data_points(df, attr_dict):
             selection_list = selection_list[:2] + [selection_list[3]]
             legend_list = [[], to_set(df, selection_list[2]).tolist()]
 
-        #legend_list = [to_set(df, selection_list[2]).tolist()]
+        # legend_list = [to_set(df, selection_list[2]).tolist()]
         data_points_list = [get_Col_OR_Shape(df, selection_list[2], selection_list[0]),
-                             get_Col_OR_Shape(df, selection_list[2], selection_list[1])]
+                            get_Col_OR_Shape(df, selection_list[2], selection_list[1])]
         return labels_list, data_points_list, legend_list
 
-def sorted_data_points(df,attr_dict,sort_attr,sort_level):
 
-    return data_points(sort_df(df,sort_attr),attr_dict)
+def sorted_data_points(df, attr_dict, sort_attr):
+    return data_points(sort_df(df, sort_attr), attr_dict)
 
 
-#new helper functions
+# new helper functions
 def getCaseIndex(df):
-    pattern = re.compile(".*(C|c)ase.*") 
+    pattern = re.compile(".*(C|c)ase.*")
     caseLabel = -1
     caseIndex = None
     caseFoundList = []
 
     for col in df.columns:
         match = pattern.match(col)
-    
+
         if match is None:
             pass
         else:
             caseLabel = match.group()
             caseFoundList.append(caseLabel)
-     
-    if(len(caseFoundList)== 1):
+
+    if (len(caseFoundList) == 1):
         caseIndex = df.columns.get_loc(caseLabel)
-    if(len(caseFoundList) > 1):
+    if (len(caseFoundList) > 1):
         caseIndex = df.columns.get_loc("case:concept:name")
         caseLabel = "case:concept:name"
-    if(caseIndex == -1):
+    if (caseIndex == -1):
         print("ERRRRORRRRR")
     return caseIndex
 
-def getCaseLabel(df):
-    pattern = re.compile(".*(C|c)ase.*") 
-    caseLabel = -1
-    caseIndex = None
-    caseFoundList = []
 
-    for col in df.columns:
-        match = pattern.match(col)
-    
-        if match is None:
-            pass
-        else:
-            caseLabel = match.group()
-            caseFoundList.append(caseLabel)
-     
-    if(len(caseFoundList)== 1):
-        caseIndex = df.columns.get_loc(caseLabel)
-    if(len(caseFoundList) > 1):
-        caseIndex = df.columns.get_loc("case:concept:name")
-        caseLabel = "case:concept:name"
-    if(caseIndex == -1):
-        print("ERRRRORRRRR")
-    return caseLabel
+def getCaseLabel(df):
+    return df.columns[getCaseIndex(df)]
+
 
 def getTimeLabel(df):
-    pattern = re.compile(".*(T|t)ime.*") 
-    timeLabel = None
-    timeIndex = None
-    timeFoundList = []
-    match = None
+    return df.columns[getTimeIndex(df)]
 
-    for col in df.columns:
-        match = pattern.match(col)
-        
-        if match is None:
-            pass
-        else:
-            timeLabel = match.group()
-            timeFoundList.append(timeLabel)
-     
-    if(len(timeFoundList)== 1):
-        timeIndex = df.columns.get_loc(timeLabel)
-        timeLabel = df.columns[timeIndex]
-    if(len(timeFoundList) > 1):
-        timeIndex = df.columns.get_loc("time:timestamp")
-        timeLabel = "time:timestamp"
-    if(timeIndex == -1):
-        print("ERRRRORRRRR")
-    return timeLabel
 
 def getTimeIndex(df):
-    pattern = re.compile(".*(T|t)ime.*") 
+    pattern = re.compile(".*(T|t)ime.*")
     timeLabel = -1
     timeIndex = None
     timeFoundList = []
@@ -180,24 +144,20 @@ def getTimeIndex(df):
 
     for col in df.columns:
         match = pattern.match(col)
-        
+
         if match is None:
             pass
         else:
             timeLabel = match.group()
             timeFoundList.append(timeLabel)
-     
-    if(len(timeFoundList)== 1):
+
+    if (len(timeFoundList) == 1):
         timeIndex = df.columns.get_loc(timeLabel)
-    if(len(timeFoundList) > 1):
+    if (len(timeFoundList) > 1):
         timeIndex = df.columns.get_loc("time:timestamp")
         timeLabel = "time:timestamp"
-    if(timeIndex == -1):
+    if (timeIndex == -1):
         print("ERRRRORRRRR")
     return timeIndex
-
-
-
-
 
 
